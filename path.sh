f7cf3343f84deea74a428a42c71b9a33b4ff1aa0
Snowdar <snowdar@stu.xmu.edu.cn>
2020-04-15 23:50:46 -0400

    environment

--- path.sh
+++ path.sh
@@ -1,7 +1,15 @@
 # Auto-config environment by Snowdar (2020-04-16)
 
+# Note: please make sure your project path is just like kaldi/egs/xmuspeech/yourproject, where
+# the project should be in sub-sub-sub-dir of kaldi root. If not, modify KALDI_ROOT by yourself.
+
+# Use decode_symbolic_link.sh rather than ../../../ to get the KALDI_ROOT so that it could
+# support the case that the project is linked by a symbolic and $PWD contains the symbolic.
+
 current_dir=$(subtools/linux/decode_symbolic_link.sh $PWD)
 kaldi_root_dir=$(dirname $(dirname $(dirname $current_dir)))
+
+[ ! -d $KALDI_ROOT/tools ] && echo >&2 "[KALDI_ROOT ERROR] Got an invalid path $KALDI_ROOT when source environment (See the 'Note' in subtools/path.sh to correct it by yourself)." && exit 1
 
 export KALDI_ROOT=$kaldi_root_dir
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
