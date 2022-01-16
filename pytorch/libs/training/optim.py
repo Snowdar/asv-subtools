fix loss heated up, update Lookahead optim

pytorch/libs/training/optim.py
@@ -6,6 +6,7 @@ import logging
 import types
 import math
 import itertools as it
+from collections import defaultdict
 from torch._six import inf
 from functools import partial, wraps
 import warnings
@@ -96,52 +97,100 @@ def get_optimizer(model, params:dict={}):
 
 ## Optim-wrapper ✿
 class Lookahead(Optimizer):
-    """https://github.com/lonePatient/lookahead_pytorch/blob/master/optimizer.py
-    """
-    def __init__(self, base_optimizer, k=5, alpha=0.5):
+    '''
+    PyTorch implementation of the lookahead wrapper.
+    Lookahead Optimizer: https://arxiv.org/abs/1907.08610
+    https://github.com/lonePatient/lookahead_pytorch/blob/master/optimizer.py
+
+    '''
+    def __init__(self, optimizer,alpha=0.5, k=6,pullback_momentum="none"):
+        '''
+        :param optimizer:inner optimizer
+        :param k (int): number of lookahead steps
+        :param alpha(float): linear interpolation factor. 1.0 recovers the inner optimizer.
+        :param pullback_momentum (str): change to inner optimizer momentum on interpolation update
+        '''
         if not 0.0 <= alpha <= 1.0:
             raise ValueError(f'Invalid slow update rate: {alpha}')
         if not 1 <= k:
             raise ValueError(f'Invalid lookahead steps: {k}')
-        self.optimizer = base_optimizer
+        self.optimizer = optimizer
         self.param_groups = self.optimizer.param_groups
         self.alpha = alpha
         self.k = k
-        self.is_back_step = False
-        self.init_weights = False
+        self.step_counter = 0
+        assert pullback_momentum in ["reset", "pullback", "none"]
+        self.pullback_momentum = pullback_momentum
+        self.state = defaultdict(dict)
 
-        for group in self.param_groups:
-            group["step_counter"] = 0
+        # Cache the current optimizer parameters
+        for group in self.optimizer.param_groups:
+            for p in group['params']:
+                param_state = self.state[p]
+                param_state['cached_params'] = torch.zeros_like(p.data)
+                param_state['cached_params'].copy_(p.data)
+
+    def __getstate__(self):
+        return {
+            'state': self.state,
+            'optimizer': self.optimizer,
+            'alpha': self.alpha,
+            'step_counter': self.step_counter,
+            'k':self.k,
+            'pullback_momentum': self.pullback_momentum
+        }
+
+    def zero_grad(self):
+        self.optimizer.zero_grad()
+
+    def state_dict(self):
+        return self.optimizer.state_dict()
+
+    def load_state_dict(self, state_dict):
+        self.optimizer.load_state_dict(state_dict)
+
+    def _backup_and_load_cache(self):
+        """Useful for performing evaluation on the slow weights (which typically generalize better)
+        """
+        for group in self.optimizer.param_groups:
+            for p in group['params']:
+                param_state = self.state[p]
+                param_state['backup_params'] = torch.zeros_like(p.data)
+                param_state['backup_params'].copy_(p.data)
+                p.data.copy_(param_state['cached_params'])
+
+    def _clear_and_load_backup(self):
+        for group in self.optimizer.param_groups:
+            for p in group['params']:
+                param_state = self.state[p]
+                p.data.copy_(param_state['backup_params'])
+                del param_state['backup_params']
 
     def step(self, closure=None):
-        self.is_back_step = False
-        # Init weights after model in a certrain device and keep the device of weights same to model. [Snowdar 2018-09-01]
-        if not self.init_weights and self.alpha > 0:
-            self.slow_weights = [[p.clone().detach() for p in group['params']]
-                                    for group in self.param_groups]
+        """Performs a single Lookahead optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = self.optimizer.step(closure)
+        self.step_counter += 1
 
-            for w in it.chain(*self.slow_weights):
-                w.requires_grad = False
-            
-            self.init_weights = True
+        if self.step_counter >= self.k:
+            self.step_counter = 0
+            # Lookahead and cache the current optimizer parameters
+            for group in self.optimizer.param_groups:
+                for p in group['params']:
+                    param_state = self.state[p]
+                    p.data.mul_(self.alpha).add_(1.0 - self.alpha, param_state['cached_params'])  # crucial line
+                    param_state['cached_params'].copy_(p.data)
+                    if self.pullback_momentum == "pullback":
+                        internal_momentum = self.optimizer.state[p]["momentum_buffer"]
+                        self.optimizer.state[p]["momentum_buffer"] = internal_momentum.mul_(self.alpha).add_(
+                            1.0 - self.alpha, param_state["cached_mom"])
+                        param_state["cached_mom"] = self.optimizer.state[p]["momentum_buffer"]
+                    elif self.pullback_momentum == "reset":
+                        self.optimizer.state[p]["momentum_buffer"] = torch.zeros_like(p.data)
 
-        loss = None
-        if closure is not None:
-            loss = closure()
-        loss = self.optimizer.step()
-        if self.alpha > 0:
-            for group,slow_weights in zip(self.param_groups,self.slow_weights):
-                group['step_counter'] += 1
-                if group['step_counter'] % self.k != 0:
-                    continue
-                else:
-                    self.is_back_step = True
-
-                for p,q in zip(group['params'],slow_weights):
-                    if p.grad is None:
-                        continue
-                    q.data.add_(self.alpha * (p.data - q.data))
-                    p.data.copy_(q.data)
         return loss
 
 